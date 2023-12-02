1b42f742621dd1511dd46cc83eb1bb46addc99ac
Slhm <martyzno@gmail.com>
2023-12-02 01:37:58 +0100

    day1 complete, but ugly and not optimal

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let result = part1(input);
     let result2 = part2(input);
     println!("Part 1: {}", result);
-    println!("Part 2: {}", result2); // under 54888 over 
+    println!("Part 2: {}", result2);
 }
 
 fn extract_numbers_part1(input: &str) -> String {
@@ -20,58 +20,22 @@ fn extract_numbers_part1(input: &str) -> String {
 }
 
 fn convert_string_numbers(input: &str) -> String {
-    const NUMBERS_AS_STRINGS: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const START_OF_NUMBERS: [char; 7] = ['z', 'o', 't', 'f', 's', 'e', 'n'];
-    const NUMBERS_AS_STRINGS_REV: [&str; 10] = ["ero", "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"];
-    const START_OF_NUMBERS_REV: [char; 6] = ['o', 'e', 'r', 'x', 'n', 't'];
-    let mut converted_numbers = input.to_string();
-    let mut first_number = String::new();
-    let mut last_number = String::new();
-    'outer: for i in 0..converted_numbers.len() {
-        if converted_numbers.chars().nth(i).unwrap().is_ascii_digit() {
-            first_number = converted_numbers.chars().nth(i).unwrap().to_string();
-            break;
-        }
-        else {
-            if START_OF_NUMBERS.contains(&converted_numbers.chars().nth(i).unwrap()) {
-                '_inner: for j in 0..NUMBERS_AS_STRINGS.len() { 
-                    if converted_numbers.split_at(i).1.starts_with(NUMBERS_AS_STRINGS[j]) { // bug: denne sjekker alltid hele strengen
-                        first_number = j.to_string();
-                        converted_numbers = converted_numbers.split_at(i + NUMBERS_AS_STRINGS[j].len() - 1).1.to_string();
-                        break 'outer;
-                    }
-                }
-            }
-        }
-    }
-    let reversed_string = converted_numbers.chars().rev().collect::<String>();
-    'outer: for i in 0..reversed_string.len() {
-        if reversed_string.chars().nth(i).unwrap().is_ascii_digit() {
-            last_number = reversed_string.chars().nth(i).unwrap().to_string();
-            break;
-        }
-        else {
-            if START_OF_NUMBERS_REV.contains(&reversed_string.chars().nth(i).unwrap()) {
-                '_inner: for j in 0..NUMBERS_AS_STRINGS_REV.len() {
-                    if reversed_string.starts_with(NUMBERS_AS_STRINGS_REV[j]) {
-                        last_number = j.to_string();
-                        break 'outer;
-                    }
-                }
-            }
-        }
-    }
-    format!("{}{}", first_number, last_number)
-}
-
-fn convert_string_numbers_backup(input: &str) -> String {
     let numbers_as_strings: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let mut converted_numbers = input.to_string();
-    for i in 0..numbers_as_strings.len() {
-        let tmp = converted_numbers.replace(numbers_as_strings[i], &i.to_string());
-        converted_numbers = tmp;
+    let converted_numbers = input.to_string();
+    let mut all_numbers: String = String::new();
+    for i in 0..converted_numbers.len() {
+        if converted_numbers.chars().nth(i).unwrap().is_ascii_digit() {
+            all_numbers.push(converted_numbers.chars().nth(i).unwrap());
+        }
+        else {
+            for j in 0..numbers_as_strings.len() { 
+                if converted_numbers.split_at(i).1.starts_with(numbers_as_strings[j]) {
+                    all_numbers.push(j.to_string().chars().nth(0).unwrap());
+                }
+            }
+        }
     }
-    converted_numbers
+    all_numbers
 }
 
 fn extract_numbers_part2(input: &str) -> String {
@@ -142,13 +106,13 @@ mod tests {
     #[test]
     fn test_filter_numbers_part2() {
         let test_input1 = "ogtwojeoid3zero6oigjdf9gfivefive";
-        assert_eq!(convert_string_numbers(&test_input1), "25");
+        assert_eq!(convert_string_numbers(&test_input1), "2306955");
     }
 
     #[test]
     fn test_filter_numbers_part2222() {
         let test_input1 = "ogtwojeoid3zero6oigjdf9gfivefive";
-        assert_eq!(extract_numbers_part2(&test_input1), "25");
+        assert_eq!(extract_numbers_part2(&test_input1), "2306955");
     }
 
     #[test]
@@ -176,12 +140,12 @@ mod tests {
     #[test]
     fn test_bug1() {
         let test_input1 = "zoneight234";
-        assert_eq!(convert_string_numbers(&test_input1), "14");
+        assert_eq!(convert_string_numbers(&test_input1), "18234");
     }
 
     #[test]
     fn test_bug2() {
         let test_input1 = "zoneight";
-        assert_eq!(convert_string_numbers(&test_input1), "11");
+        assert_eq!(convert_string_numbers(&test_input1), "18");
     }
 }
